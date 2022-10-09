0569e0c63c75b357545948ef02f4b3eb597e112b
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-10-09 14:52:39 -0500

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
+import { useEffect, useState } from "react";
+import {data} from "../mocks/data"
 // create your App component here
+function App() {
+    const [dogBreed, setDogBreed] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false);
+
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data.message)
+            setDogBreed(data.message);
+            setIsLoaded(true)
+        });
+    }, []);
+
+    if(!isLoaded) return <p>Loading...</p>;
+
+    return <img src={dogBreed} alt="A Random Dog" />
+}
+
+export default App;
